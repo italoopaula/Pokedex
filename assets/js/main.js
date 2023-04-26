272c97928a744f4b0809bbd167462f162ce3138b
Italo Paula <italo76g@gmail.com>
2023-04-26 10:45:14 -0300

    page 1 finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,20 @@
+const pokemonList = document.getElementById('pokemonList');
+const loadMoreButton = document.getElementById('loadMoreButton');
+const maxRecords = 151;
+const limit = 10;
+let offset = 0;
+
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon ${pokemon.type}">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
-
+            
             <div class="detail">
                 <ol class="types">
                     ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
-
+            
                 <img src="${pokemon.photo}"
                     alt="${pokemon.name}">
             </div>
@@ -16,11 +22,29 @@ function convertPokemonToLi(pokemon) {
     `
 }
 
-const pokemonList = document.getElementById('pokemonList');
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        //transforma um pokemon em um item de lista de HTML e junta todos os itens em uma só lista
+        const newHtml = pokemons.map(convertPokemonToLi).join('')
+        pokemonList.innerHTML += newHtml;
+    
+    })
+}
 
-pokeApi.getPokemons().then((pokemons = []) => {
-    //transforma um pokemon em um item de lista de HTML e junta todos os itens em uma só lista
-    const newHtml = pokemons.map(convertPokemonToLi).join('')
-    pokemonList.innerHTML = newHtml;
+loadPokemonItens(offset, limit);
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit;
+    const qtdRecordsWithPageNexPage = offset + limit;
+
+    if (qtdRecordsWithPageNexPage >= maxRecords) {
+        const newLimit = maxRecords - offset;
+        loadPokemonItens(offset, newLimit);
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton); //remove o botão quando chegar no número máximo de pokemons
+    } else {
+        loadPokemonItens(offset, limit);
+    }
+
 
 })
