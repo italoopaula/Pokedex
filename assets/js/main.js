ebfe9f7130e726851a2f867aaffdd6af29187a22
Italo Paula <italo76g@gmail.com>
2023-04-25 22:39:08 -0300

    inniitial commit

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
+                </ol>
+
+                <img src="${pokemon.photo}"
+                    alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList');
+
+pokeApi.getPokemons().then((pokemons = []) => {
+    //transforma um pokemon em um item de lista de HTML e junta todos os itens em uma só lista
+    const newHtml = pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML = newHtml;
+
+})
